97b314e26fe20df85f9d725502130e2a84238f02
azzumar247 <azzumar.tbh@gmail.com>
2024-12-14 01:00:36 +0800

    added firebase and clerk integration

--- app/(routes)/dashboard/_components/Header.jsx
+++ app/(routes)/dashboard/_components/Header.jsx
@@ -1,46 +1,44 @@
-"use client";
-import React, { useEffect } from 'react';
+"use client"
+import Logo from '@/app/_components/Logo'
 import { db } from '@/config/firebaseConfig';
+import { OrganizationSwitcher, UserButton, useAuth, useUser } from '@clerk/nextjs'
 import { doc, setDoc } from 'firebase/firestore';
-import { useUser } from '@clerk/nextjs';
-import Logo from '@/app/_components/Logo';
-import { OrganizationSwitcher, UserButton } from '@clerk/nextjs';
+import React, { useEffect } from 'react'
 
 function Header() {
+  const { orgId } = useAuth();
   const { user } = useUser();
 
   useEffect(() => {
-    if (user) {
-      saveUserData();
-    }
-  }, [user]);
+    user && saveUserData();
+  }, [user])
 
+  /**
+   * Used to save user data
+   */
   const saveUserData = async () => {
-    const docId = user?.primaryEmailAddress?.emailAddress;
-    const role = 'user'; // Default role
-
+    const docId = user?.id
     try {
       await setDoc(doc(db, 'users', docId), {
         name: user?.fullName,
         avatar: user?.imageUrl,
-        email: user?.primaryEmailAddress?.emailAddress,
-        role: role,  // Default to 'user'
-      });
-    } catch (e) {
-      console.error("Error saving user data:", e);
+        email: user?.primaryEmailAddress?.emailAddress
+      })
     }
-  };
+    catch (e) {
 
+    }
+  }
   return (
-    <div className='flex justify-between items-center p-3 shadow-sm'>
+    <div className='flex justify-between items-center p-3
+    shadow-sm'>
       <Logo />
       <OrganizationSwitcher
         afterLeaveOrganizationUrl={'/dashboard'}
-        afterCreateOrganizationUrl={'/dashboard'} 
-      />
+        afterCreateOrganizationUrl={'/dashboard'} />
       <UserButton />
     </div>
-  );
+  )
 }
 
-export default Header;
+export default Header
--- app/(routes)/dashboard/page.jsx
+++ app/(routes)/dashboard/page.jsx
@@ -3,18 +3,42 @@
 import React, { useEffect } from 'react'
 import Header from './_components/Header'
 import WorkspaceList from './_components/WorkspaceList'
+import { auth, FirebaseUI } from '@/config/firebaseConfig'
+import { useAuth } from '@clerk/nextjs'
+import { signInWithCustomToken } from 'firebase/auth'
 
 
 function Dashboard() {
-  
-  return (
-    <div>
-     
-      <Header/>
+  const { getToken, userId } = useAuth()
+  const signIntoFirebaseWithClerk = async () => {
+    const token = await getToken({ template: 'integration_firebase' })
 
-      <WorkspaceList/>
-    </div>
-  )
+    const userCredentials = await signInWithCustomToken(auth, token || '')
+    // The userCredentials.user object can call the methods of
+    // the Firebase platform as an authenticated user.
+    console.log('User:', userCredentials.user)
+  }
+
+  useEffect(() => {
+    signIntoFirebaseWithClerk()
+  }, [userId])
+
+
+  if (!userId) {
+    return (
+      <div>loading...</div>
+    )
+  } else {
+    return (
+
+      <div>
+        <Header />
+        <WorkspaceList />
+      </div>
+
+    )
+  }
+
 }
 
 export default Dashboard
