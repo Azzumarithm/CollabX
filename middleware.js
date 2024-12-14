9e993c591afbb7b242be4ce196f7da7cf85962cb
azzumar247 <azzumar.tbh@gmail.com>
2024-12-14 21:46:24 +0800

    make api/webhooks public

--- middleware.js
+++ middleware.js
@@ -1,10 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// Define public routes (no authentication needed)
-const isPublicRoute = createRouteMatcher([
-  '/api/webhooks(.*)', // Webhook routes should be public
-]);
-
 // Define protected routes (authentication required)
 const isProtectedRoute = createRouteMatcher([
   '/dashboard(.*)',
@@ -12,10 +7,8 @@ const isProtectedRoute = createRouteMatcher([
 ]);
 
 export default clerkMiddleware((auth, req) => {
-  if (isPublicRoute(req)) {
-    // Disable authentication for public routes
-    auth().protect(false);
-  } else if (isProtectedRoute(req)) {
+
+  if (isProtectedRoute(req)) {
     // Enable authentication for protected routes
     auth().protect();
   }
