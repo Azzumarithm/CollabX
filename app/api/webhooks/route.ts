17b6633ac040e5b21a72be05c75fd491ad6e9086
azzumar247 <azzumar.tbh@gmail.com>
2024-12-15 11:24:53 +0800

    include clientIP and userAgent in session data

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -4,8 +4,6 @@ import { WebhookEvent } from "@clerk/nextjs/server";
 import { doc, setDoc, deleteDoc } from "firebase/firestore";
 import { db } from "@/config/firebaseConfig";
 export async function POST(req: Request) {
-
-  
   const SIGNING_SECRET = process.env.SIGNING_SECRET;
 
   if (!SIGNING_SECRET) {
@@ -34,7 +32,6 @@ export async function POST(req: Request) {
   const payload = await req.json();
   console.log("Raw Payload:", JSON.stringify(payload, null, 2));
   const body = JSON.stringify(payload);
-
 
   let evt: WebhookEvent;
 
@@ -75,33 +72,39 @@ export async function POST(req: Request) {
         status: evt.data.status,
         updatedAt: evt.data.updated_at,
         userId: evt.data.user_id,
-
+        clientIp: evt["event_attributes"]["http_request"]["client_ip"],
+        userAgent: evt["event_attributes"]["http_request"]["user_agent"],
       });
       console.log(`Session created for user: ${user_id}`);
-    } else if (eventType === "session.ended") {
+    } else if (
+      eventType === "session.ended" ||
+      eventType === "session.removed"
+    ) {
       // Update session status when a session ends
       const { user_id } = evt.data;
-      await setDoc(userLogRef, {
-        abandonAt: evt.data.abandon_at,
-        clientId: evt.data.client_id,
-        createdAt: evt.data.created_at,
-        expireAt: evt.data.expire_at,
-        id: evt.data.id,
-        lastActiveAt: evt.data.last_active_at,
-        object: evt.data.object,
-        status: evt.data.status,
-        updatedAt: evt.data.updated_at,
-        userId: evt.data.user_id,
-        
-      }, { merge: true });
+      await setDoc(
+        userLogRef,
+        {
+          abandonAt: evt.data.abandon_at,
+          clientId: evt.data.client_id,
+          createdAt: evt.data.created_at,
+          expireAt: evt.data.expire_at,
+          id: evt.data.id,
+          lastActiveAt: evt.data.last_active_at,
+          object: evt.data.object,
+          status: evt.data.status,
+          updatedAt: evt.data.updated_at,
+          userId: evt.data.user_id,
+          clientIp: evt["event_attributes"]["http_request"]["client_ip"],
+          userAgent: evt["event_attributes"]["http_request"]["user_agent"],
+        },
+        { merge: true }
+      );
       console.log(`Session ended for user: ${user_id}`);
-    } else if (
-      eventType === "session.removed" ||
-      eventType === "session.revoked"
-    ) {
+    } else if (eventType === "session.revoked") {
       // Delete session data when a session is removed or revoked
       await deleteDoc(userLogRef);
-      console.log(`Session removed/revoked for user`);
+      console.log(`Session revoked for user`);
     }
 
     return new Response("Webhook received and processed", { status: 200 });
